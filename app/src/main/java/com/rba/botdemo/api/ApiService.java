5c1e294fb3a76089cf5c089ff8edf5c30e9d4552
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-06 15:28:31 -0500

    feat(chat): callback added

--- app/src/main/java/com/rba/botdemo/api/ApiService.java
+++ app/src/main/java/com/rba/botdemo/api/ApiService.java
@@ -1,8 +1,15 @@
 package com.rba.botdemo.api;
 
 import com.rba.botdemo.BuildConfig;
+import com.rba.botdemo.model.response.ChatResponse;
 import com.rba.botdemo.model.response.SynchronizeResponse;
+
+import java.util.HashMap;
+
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import rx.Observable;
 
 /**
@@ -13,5 +20,9 @@ public interface ApiService {
 
     @GET(BuildConfig.URL_SYNCHRONIZE)
     Observable<SynchronizeResponse> getSynchronize();
+
+    @FormUrlEncoded
+    @POST(BuildConfig.URL_MESSAGE)
+    Observable<ChatResponse> postMessage(@FieldMap HashMap<String, String> data);
 
 }
---
+++ app/src/main/java/com/rba/botdemo/chat/ChatCallback.java
@@ -0,0 +1,19 @@
+package com.rba.botdemo.chat;
+
+import com.rba.botdemo.api.NetworkError;
+import com.rba.botdemo.model.response.ChatResponse;
+import com.rba.botdemo.model.response.ErrorResponse;
+
+/**
+ * Created by Ricardo Bravo on 6/02/17.
+ */
+
+public interface ChatCallback {
+
+    void onChatResponse(ChatResponse chatResponse);
+
+    void onChatError(ErrorResponse errorResponse);
+
+    void onChatFailure(NetworkError networkError);
+
+}
